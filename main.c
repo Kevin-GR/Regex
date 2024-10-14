b3c1d00cc75998ddb5879f405b0f2f381bc9ae2d
Kevin-GR <kegure112@gmail.com>
2024-10-14 10:44:38 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
+#include "main.h"
+
+int main()
+{
+  FILE *grammar = fopen("Gramatica.txt", "r");
+if(grammar == NULL)
+{
+  perror("Error opening the file");
+  return EXIT_FAILURE;
+}
+
+Node *head = createLindekList(grammar);
+fclose(grammar);
+printList(Head);
+freeLinkedList(head);
+}
+
 
